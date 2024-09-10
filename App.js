13751a3bf2af8b7fc1aafcee8712c37ff446d1c7
lisliaxx <l1875150880@gmail.com>
2024-09-10 15:53:40 -0600

    added Input.js to seperate the textInput

--- App.js
+++ App.js
@@ -1,28 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Header from './Components/Header';
-import { useState } from 'react';
+import Input from './Components/Input'; 
 
 export default function App() {
-  const [text, setText] = useState('');
   const appName = "My App!";
-  function updateText(changeText){
-    // console.log(changeText);
-    setText(changeText);
-  }
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Header name={appName} />
-      <TextInput 
-        placeholder='Type Something'
-        autoCorrect={true}
-        keyboardType='default'
-        value = {text}
-        style ={{borderBottomColor: 'black', borderBottomWidth: 2}}
-        onChangeText={updateText}
-      />
-      <Text>{text}</Text>
+      <Input />
+      {/* <Text>{text}</Text> */}
     </View>
   );
 }
