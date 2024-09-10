40523b970b02442e57e4853dca90debb9eed1865
lisliaxx <l1875150880@gmail.com>
2024-09-10 15:47:33 -0600

    added TextInput and show what the user is typing

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import Header from './Components/Header';
+import { useState } from 'react';
 
 export default function App() {
+  const [text, setText] = useState('');
   const appName = "My App!";
+  function updateText(changeText){
+    // console.log(changeText);
+    setText(changeText);
+  }
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Header name={appName} />
+      <TextInput 
+        placeholder='Type Something'
+        autoCorrect={true}
+        keyboardType='default'
+        value = {text}
+        style ={{borderBottomColor: 'black', borderBottomWidth: 2}}
+        onChangeText={updateText}
+      />
+      <Text>{text}</Text>
     </View>
   );
 }
