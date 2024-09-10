827ae788b20a7e9f4897a234143ae9fbaa0bf4e4
lisliaxx <l1875150880@gmail.com>
2024-09-10 14:47:27 -0600

    added a variable name for the app name and used it in the Text component

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const appName = "My App!";
   return (
     <View style={styles.container}>
-      <Text>Welcome to the app</Text>
+      <Text>Welcome to {appName}</Text>
       <StatusBar style="auto" />
     </View>
   );
