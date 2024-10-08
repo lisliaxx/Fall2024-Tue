c3fc246079cf21c395ca2c99fc59bfb0b3b3b6a0
lisliaxx <li.tingyu@northeastern.edu>
2024-10-08 14:01:56 -0700

    updated pressable inside of the view

--- Components/GoalItem.js
+++ Components/GoalItem.js
@@ -1,4 +1,4 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View, Pressable } from "react-native";
 import React from "react";
 import { useNavigation } from '@react-navigation/native';
 
@@ -8,20 +8,28 @@ export default function GoalItem({item, deleteHandler}) {
     function handleDelete() {
         console.log("Delete button pressed", item.id);
         deleteHandler(item.id);
-      }
+    }
 
     function handlePress() {
-      console.log("Navigate to GoalDetails", item);
-      navigation.navigate('GoalDetails', {goalItem: item});
+        console.log("Navigate to GoalDetails", item);
+        navigation.navigate('GoalDetails', {goalItem: item});
     }
 
     return (
         <View key={item.id} style={styles.textContainer}>
-          <Text style={styles.text}>{item.text}</Text>
-          <Button title="X" onPress={handleDelete} />
-          <Button title="i" onPress={handlePress} />
+            <Pressable 
+                onPress={handlePress} 
+                style={({pressed}) => [
+                    styles.horizontalContainer, 
+                    pressed && styles.pressableStyle
+                ]} 
+                android_ripple={{color:'red', radius:25}}
+            >
+                <Text style={styles.text}>{item.text}</Text>
+                <Button title="X" onPress={handleDelete} />
+            </Pressable>
         </View>
-      );
+    );
 }
 
 const styles = StyleSheet.create({
@@ -29,12 +37,20 @@ const styles = StyleSheet.create({
         fontSize: 30,
         color: 'green',
         padding: 10,
-      },
-      textContainer: {
+    },
+    textContainer: {
         flexDirection: 'row',
         alignItems: 'center',
         backgroundColor: 'white',
         borderRadius: 5,
         marginTop: 15,
-      },
-    });
+    },
+    horizontalContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    pressableStyle: {
+        opacity: 0.5,
+        backgroundColor: 'red',
+    },
+});
