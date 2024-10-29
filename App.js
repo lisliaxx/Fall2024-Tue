0d0955ff3160947af9be1708705a8e01e2b9a151
lisliaxx <li.tingyu@northeastern.edu>
2024-10-29 14:39:31 -0700

    Seperating auth screens with app screens as another security layer to show home screen only when log in

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
 import {View, Text, Button} from 'react-native';
 import React from 'react';
+import { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from './Components/Home';
 import GoalDetails from './Components/GoalDetails';
 import Login from './Components/Login';
 import Signup from './Components/Signup';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 
 const Stack = createStackNavigator();
-console.log(Stack);
+// console.log(Stack);
+
+const authStack = 
+<>
+  <Stack.Screen name="Signup" component={Signup} />
+  <Stack.Screen name="Login" component={Login} />
+</>
+
+const appStack =
+<>
+  <Stack.Screen name="Home" component={Home} />
+  <Stack.Screen name="GoalDetails" component={GoalDetails} />
+</>
 
 export default function App() {
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+  useEffect(() => {onAuthStateChanged(getAuth(), (user) => {
+    if (user) {
+      setIsUserLoggedIn(true);
+    } else {
+      setIsUserLoggedIn(false);
+    }
+  });
+  }
+  , []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Login"
         screenOptions={{
           headerStyle: {
             backgroundColor: '#80d195',
@@ -25,34 +49,7 @@ export default function App() {
             fontWeight: 'bold',
           },
         }}>
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{
-            title: 'Login',
-          }}
-        />
-        <Stack.Screen 
-          name="Home" 
-          component={Home} 
-          options={{ 
-            title : "My Goals",
-          }}
-        />
-        <Stack.Screen 
-          name="GoalDetails" 
-          component={GoalDetails} 
-          options={({route}) => ({
-            title: route.params?.goalItem?.txt || "Goal Details",
-          })}
-        />
-        <Stack.Screen
-          name="Signup"
-          component={Signup}
-          options={{
-            title: 'Signup',
-          }}
-        />
+        {isUserLoggedIn ? appStack : authStack}
       </Stack.Navigator>
     </NavigationContainer>
   );
